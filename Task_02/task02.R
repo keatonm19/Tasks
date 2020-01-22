f563fa82370323d872a04ad4260e86532738d2d0
keatonm19 <59904107+keatonm19@users.noreply.github.com>
2020-01-22 10:05:06 -0500

    task02a 2

--- Task_02/task02.R
+++ Task_02/task02.R
@@ -2,3 +2,4 @@ setwd("~/Desktop/Evolution/Tasks/Task_02")
 
 Data <- read.csv("http://jonsmitchell.com/data/beren.csv", stringsAsFactors=T)
 
+beren3 <- read.csv("beren_new.csv", stringsAsFactors=F)
