861f578d32a8f51e933186987c305e5aaf779e22
keatonm19 <59904107+keatonm19@users.noreply.github.com>
2020-01-31 09:06:33 -0500

    <keatonm19> Task03

---
+++ Task_03/task_03.R
@@ -0,0 +1,97 @@
+setwd("~/Desktop/Evolution/Tasks/Task_03")
+#install.packages("paleobioDB", dep=T)
+library(paleobioDB)
+
+Taxon<-"Dinosauria"
+MinMA<-66
+MaxMA<-252
+
+# chance to put in little comments
+fossils<-pbdb_occurrences(base_name=Taxon, show=c("phylo", "coords", "ident"), min_ma=MinMA, max_ma=MaxMA)
+
+Res<-5
+nspeciesOverTime<-pbdb_richness(fossils, rank="genus", temporal_extent=c(MaxMA,MinMA), res=Res)
+
+# 37species are found
+
+par(mar=c(4,5,2,1), las=1, tck=-0.01, mgp=c(2.5, 0.5, 0))
+plot(seq(to=MaxMA, from=MinMA, length.out=nrow(nspeciesOverTime)), nspeciesOverTime[,2], xlim=c(MaxMA, MinMA), type="l", xlab="age(millions of years ago)", ylab="num. of species", main=Taxon)
+newspeciesOverTime<-pbdb_orig_ext(fossils, res=5, rank="species", temporal_extent=c(MinMA, MaxMA))
+
+par(mar=c(4,5,2,1), las=1, tck=-0.01, mgp=c(2.5,0.5,0))
+plot(seq(to=MaxMA, from=MinMA, length.out=nrow(newspeciesOverTime)), newspeciesOverTime[,1], xlim=c(MaxMA, MinMA), type="l", xlab="age(millions of years ago)" , ylab="num. of species", main=Taxon)
+lines(seq(to=MaxMA, from=MinMA, length.out=nrow(newspeciesOverTime)),  newspeciesOverTime[,2], col='red')
+legend("topleft", legend=c("first appear", "go extinct"), col=c('black', 'red'), lty=1, bty="n")
+
+# this lines follow the same types of patterns, but they obviously become extinct after they appear. Extinctions are very close to the first to appear which is something that I would not have expected but there is a bit of inaccuracy within these numbers because you don't know if they first to appear actually appeared first and so on. No, dinosaurs were not evolving the fastest. 
+OceanCol<-"light blue"
+LandCol<-"black"
+Cols<-c('#fee5d9', '#fcae91', '#fb6a4a', '#de2d26', '#a50f15')
+par(las=0)
+pbdb_map_richness(fossils, col.ocean=OceanCol, col.int=LandCol, col.rich=Cols)
+MinMA<-201
+MaxMA<-252
+triassic_fossils<-pbdb_occurrences(base_name=Taxon, show=c("phylo", "coords", "ident"), min_ma=MinMA, max_ma=MaxMA)
+MinMA<-145
+MaxMA<-201
+jurassic_fossils<-pbdb_occurrences(base_name=Taxon, show=c("phylo", "coords", "ident"), min_ma=MinMA, max_ma=MaxMA)
+MinMA<-66
+MaxMA<-145
+cretaceous_fossils<-pbdb_occurrences(base_name=Taxon, show=c("phylo", "coords", "ident"), min_ma=MinMA, max_ma=MaxMA)
+dev.new(height=7.8, width=13)
+pbdb_map_richness(triassic_fossils, col.ocean=OceanCol, col.int=LandCol, col.rich=Cols)
+mtext(side=3, "Triassic(252-201Ma)", cex=3, line=-2)
+dev.new(height=7.8, width=13)
+pbdb_map_richness(jurassic_fossils, col.ocean=OceanCol, col.int=LandCol, col.rich=Cols)
+mtext(side=3, "Jurassic(201-145Ma)", cex=3, line=-2)
+dev.new(height=7.8, width=13)
+pbdb_map_richness(cretaceous_fossils, col.ocean=OceanCol, col.int=LandCol, col.rich=Cols)
+mtext(side=3, "Cretaceous(145-66Ma)", cex=3, line=-2)
+Taxon2<-"Mammalia"
+MinMA<-66
+MaxMA<-252
+fossils2<-pbdb_occurrences(base_name=Taxon2, show=c("phylo", "coords","ident"), min_ma=MinMA, max_ma=MaxMA)
+nspeciesOverTime2<-pbdb_richness(fossils2, rank="genus", temporal_extent=c(MaxMA,MinMA), res=Res)
+par(mar=c(4,5,2,1), las=1, tck=-0.01, mgp=c(2.5, 0.5, 0))
+Col_dino<-Cols[length(Cols)]
+Col_mammal<-Cols[1]
+LineWidth<-2
+plot(seq(to=MaxMA, from=MinMA, length.out=nrow(nspeciesOverTime)), nspeciesOverTime[,2], xlim=c(MaxMA, MinMA), type="l", xlab="age(millions of years ago)", ylab="num. of species", col=Col_dino, lwd=LineWidth)
+lines(seq(to=MaxMA, from=MinMA, length.out=nrow(nspeciesOverTime2)), nspeciesOverTime2[,2], col=Col_mammal, lwd=LineWidth)
+legend("topleft", legend=c(Taxon. Taxon2), col=c(Col_dino, Col_mammal), bty="n", lwd=LineWidth)
+
+
+##### Extension
+##### Extension
+setwd("~/Desktop/Evolution/Tasks/Task_03")
+library(paleobioDB)
+
+Taxon<-"Strigiformes"
+
+# chance to put in little comments
+fossils<-pbdb_occurrences(base_name=Taxon, show=c("phylo", "coords", "ident"))
+
+head(fossils) 
+
+Res<-5
+nspeciesOverTime<-pbdb_richness(fossils, rank="genus", temporal_extent=c(MaxMA,MinMA), res=Res)
+MaxMA<-10
+MinMA<-0
+nspeciesOverTime<-pbdb_richness(fossils, rank="genus", temporal_extent=c(MaxMA,MinMA) res=Res)
+par(mar=c(4,5,2,1), las=1, tck=-0.01, mgp=c(2.5, 0.5, 0))
+plot(seq(to=MaxMA, from=MinMA, length.out=nrow(nspeciesOverTime)), nspeciesOverTime[,2], xlim=c(MaxMA,MinMA), type="l", xlab="age(millions of years ago)", ylab="num. of species", main=Taxon)
+
+Taxon2<-"Cetacea"
+fossils2<-pbdb_occurrences(base_name=Taxon2, show=c("phylo", "coords", "ident"))
+
+Res<-5
+MaxMA<-10
+MinMA<-0
+nspeciesOverTime2<-pbdb_richness(fossils2, rank="genus", res=Res)
+head(nspeciesOverTime2)
+par(mar=c(4,5,2,1), las=1, tck=-0.01, mgp=c(2.5, 0.5, 0))
+Col_dino<-Cols[length(Cols)]
+plot(seq(to=MaxMA, from=MinMA, length.out=nrow(nspeciesOverTime2)), nspeciesOverTime[,2], xlim=c(MaxMA, MinMA), type="l", xlab="age(millions of years ago)", ylab="num. of species", main=Taxon2)
+lines(seq(to=MaxMA, from=MinMA, length.out=nrow(nspeciesOverTime2)), nspeciesOverTime2[,2], lwd=LineWidth)
+legend("topleft", legend=c(Taxon, Taxon2), bty="n", lwd=LineWidth)
+plot(seq(to=MaxMA, from=MinMA, length.out=nrow(nspeciesOverTime)), nspeciesOverTime[,2], xlim=c(MaxMA, MinMA), type="l", xlab="age(millions of years ago)", ylab="num. of species", main=Taxon)
