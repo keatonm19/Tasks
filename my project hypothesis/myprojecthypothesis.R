023af38a42971da590d27a6e322b990175b847f0
keatonm19 <59904107+keatonm19@users.noreply.github.com>
2020-02-28 10:43:06 -0500

    keatonm19 my project hypothesis upload a

---
+++ my project hypothesis/myprojecthypothesis.R
@@ -0,0 +1,3 @@
+setwd("~/Desktop/Evolution/Tasks/my project hypothesis")
+
+# Hypothesis: Development of Canine Tranmissible Venereal Tumors within canine populations are more frequent in populations where immigration is high. 
