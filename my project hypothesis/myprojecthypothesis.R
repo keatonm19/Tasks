99c5ccea5dfff9fa2e335548988d80579a8e2dbe
keatonm19 <59904107+keatonm19@users.noreply.github.com>
2020-03-13 10:50:25 -0400

    keatonm19 my project hypothesis

--- my project hypothesis/myprojecthypothesis.R
+++ my project hypothesis/myprojecthypothesis.R
@@ -1,3 +1,9 @@
 setwd("~/Desktop/Evolution/Tasks/my project hypothesis")
 
-# Hypothesis: Development of Canine Tranmissible Venereal Tumors within canine populations are more frequent in populations where immigration is high. 
+# Hypothesis: Development of Canine Tranmissible Venereal Tumors within canine populations with increase in frequency with increase in size and breed of the canine.
+source("~/Desktop/Evolution/Tasks/my project hypothesis")
+object<-source("ProjectHypothesis.csv")
+
+
+write.csv(object, "ProjectHypothesis.csv", quote=T, row.names=TRUE )
+Data<-read.csv("ProjectHypothesis.csv")
