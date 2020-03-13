4aa996a73833831cb9d001b882ab6cc5143609a7
keatonm19 <59904107+keatonm19@users.noreply.github.com>
2020-03-13 08:49:08 -0400

    keatonm19 Task 08

---
+++ Task_08/08.R
@@ -0,0 +1,73 @@
+setwd("~/Desktop/Evolution/Tasks/Task_08")
+library(phytools)
+text.string<-
+"(((((((cow, pig), whale),(bat, (lemur, human))),(robin, iguana)), coelacanth), (gold_fish, trout)), shark);"
+vert.tree<-read.tree(text=text.string)
+plot(vert.tree, edge.width=2)
+
+nodelabels(framem="circle", bg='white', cex=1)
+
+
+question 1 : Sharks are more closely related
+
+
+vert.tree
+str(vert.tree)
+#Question2: no there are not lengths
+
+tree<-read.tree(text="(((A,B), (C,D)),E);")
+plotTree(tree, offset=1)
+tiplabels(frame="circle", bg='lightblue', cex=1)
+nodelabels(frame="circle", bg='white', cex=1)
+tree$tip.label
+tree$edge
+
+
+AnolisTree<-force.ultrametric(read.tree("https://jonsmitchell.com/data/anolis.tre"))
+par(las=1)
+hist(AnolisTree$edge.length, col='black', border='white', main="", xlab="edge lengths for the Anolis tree", ylim=c(0,50), xlim=c(0,6))
+
+tipEdges<-which(AnolisTree$edge[,2]<=Ntip(AnolisTree))
+Lengths<-AnolisTree$edge.length
+names(Lengths)<-AnolisTree$tip.label
+names(Lengths)[which(Lengths==min(Lengths))]
+
+plot(AnolisTree, cex=0.25)
+Labs<-sapply(AnolisTree$edge.length, round, digits=2)
+edgelabels(text=Labs, cex=0.25)
+
+
+#Question4
+roundPhylogram(vert.tree)
+
+
+#Question5
+tiplabels(frame="circle", bg='red', cex=1)
+
+Question6-9
+Lengths<-vert.tree
+names(Lengths)<-vert.tree
+names(Lengths)[which(Lengths == min(Lengths))]
+
+
+
+ltt(AnolisTree)
+abline(0,1,lwd=2, col='red', lty=2)
+#the line continues to go up but never goes down. The slope is varied throughout the graph meaning that the populations are increasing but also meets platos at different points. Also, growing at different rates.
+
+#Question 10
+fit.bd(AnolisTree, rho=0.2)
+
+install.packages("treebase")
+
+
+
+
+
+
+
+
+
+
+
+
